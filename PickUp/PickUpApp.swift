ee08e671a4393cc81724dd688c9bea3cf472d0b0
marvin753 <barsalmarvin@gmail.com>
2024-07-30 17:06:53 +0200

    Initial Commit

---
+++ PickUp/PickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUpApp.swift
+//  PickUp
+//
+//  Created by Marvin Barsal on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct PickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
